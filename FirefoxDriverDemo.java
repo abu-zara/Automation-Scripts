a95ba2f14d8471283b25df239d7aec259b608699
Ismaila Yunus A <ismail.akareem@yahoo.com>
2018-02-13 14:54:57 +0100

    Firefoxwebdriver

---
+++ FirefoxDriverDemo.java
@@ -0,0 +1,17 @@
+package basicweb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxDriverDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Abdulkarim.samrumi-boys-HP\\Downloads\\selenium-workspace\\geckodriver-v0.19.1-win64/geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseURL = "http://www.google.com";
+		
+		driver.get(baseURL);
+	}
+
+}
